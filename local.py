f7d7c3a7af342a8d3d91f65b24bb473627fc24e1
GeoffChurch <geoffrey.a.churchill@gmail.com>
2017-04-28 01:04:20 -0400

    finished draft implementation of local.py

--- local.py
+++ local.py
@@ -1,27 +1,42 @@
+from collections import namedtuple, defaultdict
+
+Message = namedtuple('Message', ['target', 'data'])
+
 class Node():
-    
+
     def __init__(self, name, scoreFunction):
         self.name = name
-        self.scoreFunction = scoreFunction
-        self.name2neighbor = dict()
-        self.name2bestRouteandScore = {}
+        self.scoreFunction = scoreFunction # [Node] => float
+        self.neighbors = []
+        self.bestRoute = dict() # bestRoute[X] returns current best route to X
         self.inputBuffer = []
-        
-    def linkTo(self, dst):
-        self.name2neighbor[dst.name] = dst
-        self.name2bestRouteandScore[dst.name] = {'path': None, 'score': -float('inf')}
+        self.outputBuffer = []
 
-    def step(self):
-        for path in self.inputBuffer:
-            neighborName = path[-1].name
-            pathScore = self.scoreFunction(path)
-            if name2bestRouteandScore[neighborName]['score'] < pathScore:
-                self.name2bestRouteandScore[neighborName]['path'] = path
-                self.name2bestRouteandScore[neighborName]['score'] = pathScore
-            
+    def linkTo(self, neighbor):
+        self.neighbors.append(neighbor)
+        self.outputBuffer.append(Message(target=neighbor, data=[self])) # queue a message to tell them we're neighbors
+        #self.name2bestRouteandScore[dst.name] = {'path': None, 'score': -float('inf')}
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+    def updateSelf(self):
+        for route in self.inputBuffer:
+            dst = route[0]
+            pathScore = self.scoreFunction(route)
+            curBestScore = self.scoreFunction(self.bestRoute[dst])
+            if curBestScore < pathScore: # higher is better
+                self.bestRoute[dst] = route
+                outputRoute = route + [self]
+                for neighbor in self.neighbors:
+                    self.outputBuffer.append(Message(target=neighbor, data=outputRoute))
+        self.inputBuffer = []
+
+    def broadcastChanges(self):
+        for message in self.outputBuffer:
+            message.target.inputBuffer.append(message.data)
+        self.outputBuffer = []
                 
-            
-        
 def local(G, scoreFunction):
     # instantiate nodes
     nodes = {name: Node(name=name, scoreFunction=scoreFunction) for name in G.getNodes()}
@@ -35,6 +50,6 @@ def local(G, scoreFunction):
     while True:
         input()
         for node in nodes:
-            node.step()
+            node.broadcastChanges()
         for node in nodes:
-            node.endStep()
+            node.updateSelf()
